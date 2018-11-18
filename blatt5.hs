e49897e33e83c8b4504ecbd10322099981199d8d
iasy <i-asy@web.de>
2018-11-18 23:31:47 +0100

    1.11

--- blatt5.hs
+++
@@ -1,5 +0,0 @@
-addx :: [a] -> [a] -> [a]
-addx [] [] = []
-addx xs [] = xs
-addx [] ys = ys
-addx (x:xs) (y:ys) = x : y : (addx xs ys)
--- test.hs
+++
@@ -1,7 +0,0 @@
--- ghci version 8.6.1 
-subseq :: [Int] -> [Int] -> Bool
--- testet ob x eine subsequenz von y ist.
--- problem: funktioniert nicht korrekt wenn x und y nur noch leere listen sind. 
-subseq x [] = False
-subseq [] y = True
-subseq (x:xs) (y:ys) = if x == y then subseq xs ys else subseq (x:xs) ys
